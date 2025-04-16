32cb3df43f460464cfe900d304c1949da419f82b
MahabubKarim <mahabub.karim21@gmail.com>
2025-04-16 15:14:48 +0600

    Dummy Native Button has been added

---
+++ composeApp/src/commonMain/kotlin/com/mmk/dailypulse/NativeButton.kt
@@ -0,0 +1,10 @@
+package com.mmk.dailypulse
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+
+@Composable
+fun NativeButton(modifier: Modifier = Modifier) {
+
+}
+
